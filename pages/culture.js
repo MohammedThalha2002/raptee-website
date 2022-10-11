bddb031a8f066bcdb67f01a01093db97b8e8f6e9
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2022-10-11 19:56:58 +0530

    commit

---
+++ pages/culture.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function culture() {
+    return (
+        <div>culture</div>
+    )
+}
+
+export default culture
--- pages/techstory.js
+++ pages/techstory.js
@@ -1,168 +1,40 @@
-import { NextPage } from 'next'
 import NavBar from '../components/NavBar'
 import ReserveBtn from '../components/ReserveBtn'
 import BottomLoader from '../components/BottomLoader'
 import SideActiveIndicator from '../components/SideActiveIndicator'
 import Image from 'next/image'
-import { useRef } from 'react'
 
-export default function techstory() {
+function techstory() {
     return (
-        <div>
-            <main className='bg-black'>
-                <NavBar></NavBar>
-                {/* Our Products SECTION */}
-                <section className='bg-black aspect-video snap-center relative' id='homepage-id'>
-                    <div className=' h-20 '></div>
-                    <div className=''>
-                        <Image 
-                            src="/images/7.png"
-                            width="1920"
-                            height="1080"
-                            priority={true}
-                        />
-                        <div className="absolute left-0 top-20 pl-14 pt-2">
-                            <div className="content text-left">
-                                <h3 className='text-4xl font-light font-oswald 
+        <div className='bg-black'>
+            <NavBar></NavBar>
+            <section className='bg-black aspect-video snap-center relative'>
+                <div className=' h-20 '></div>
+                <div className=''>
+                    <Image
+                        src="/images/7.png"
+                        width="1920"
+                        height="1080"
+                        priority={true}
+                    />
+                    <div className="absolute left-0 top-20 pl-14 pt-2">
+                        <div className="content text-left">
+                            <h3 className='text-4xl font-light font-oswald 
                                 tracking-widest mb-4'>We Conceive and Create</h3>
-                                <h1 className='text-4xl font-thin font-mont tracking-widest'>Our Products</h1>
-                                <div className="underline w-10 h-0.5 rounded-xl my-2 bg-highlight"></div>
-                                <h4 className='text-2xl font-thin font-mont tracking-widest'>
-                                    We have developed 90% of the components <br />
-                                    in-house which are best altered for our Indian <br />
-                                    roads.</h4>
-                            </div>
-                        </div>
-                        <SideActiveIndicator index={1}></SideActiveIndicator>
-                        <BottomLoader></BottomLoader>
-                    </div>
-                </section>
-                {/* ABOUT SECTION */}
-                <section className='bg-black aspect-video snap-center relative' id='about-id'>
-                    <div className="flex h-full">
-                        {/* SIDE CONTENTS */}
-                        <div className='w-2/5 flex flex-col justify-center pl-20 pr-4'>
-                            <div className="title mb-14">
-                                <h2 className=' font-oswald font-light text-4xl tracking-wide'>About</h2>
-                                <div className="underline w-10 h-0.5 rounded-xl my-2 bg-highlight"></div>
-                            </div>
-                            <div className="contents text-lg tracking-wide text-gray-500">
-                                <h3 className='hover:text-white hover:text-xl'>
-                                    We at Raptee are driven by an innovative spirit to redefine electric mobility.
-                                    We design and build motorcycles that prove that switching to electric is not
-                                    a sacrifice.
-                                </h3>
-                                <h3 className='hover:text-white hover:text-xl'>
-                                    Our motorcycles are intuitive and engineered to outperform conventionally
-                                    powered <br /> vehicles;
-                                </h3>
-                                <h3 className='hover:text-white hover:text-xl'>
-                                    they are intelligent, powerful, energy-efficient and loaded with cutting-edge
-                                    technology to augment every single ride you take.
-                                </h3>
-                            </div>
-                        </div>
-                        <div className='w-3/5'>
-                            <Image
-                                src="/images/5.png"
-                                width={1204}
-                                height={1052}
-                                priority={true}
-                            />
+                            <h1 className='text-4xl font-thin font-mont tracking-widest'>Our Products</h1>
+                            <div className="underline w-10 h-0.5 rounded-xl my-2 bg-highlight"></div>
+                            <h4 className='text-2xl font-thin font-mont tracking-widest'>
+                                We have developed 90% of the components <br />
+                                in-house which are best altered for our Indian <br />
+                                roads.</h4>
                         </div>
                     </div>
-                    <SideActiveIndicator index={2}></SideActiveIndicator>
+                    <SideActiveIndicator index={1}></SideActiveIndicator>
                     <BottomLoader></BottomLoader>
-                </section>
-                {/* ZERO SECTIONS */}
-                <section className='bg-black aspect-video snap-center relative' id='zero-id'>
-                    <div className="flex h-full">
-                        <div className='w-2/6 flex flex-col justify-center pl-20 pr-28'>
-                            <div className="title mb-14 h-28">
-                                <h2 className=' font-oswald font-light text-4xl tracking-wide'>Zero</h2>
-                                <div className="h-12 overflow-hidden">
-                                    <span className='block h-full animate-spin_words font-oswald font-light text-4xl tracking-wide'>Hassle</span>
-                                    <span className='block h-full animate-spin_words font-oswald font-light text-4xl tracking-wide'>Emissions</span>
-                                    <span className='block h-full animate-spin_words font-oswald font-light text-4xl tracking-wide'>Maintenence</span>
-                                </div>
-                                <div className="underline w-10 h-0.5 rounded-xl my-2 bg-highlight"></div>
-                            </div>
-                            {/* CONTENTS */}
-                            <div className="h-60 overflow-hidden">
-                                <span className="block h-full animate-spin_words text-lg tracking-wide text-gray-500 ">
-                                    <h3 className='hover:text-white hover:text-xl'>
-                                        Breakdowns, failures, and repairs are all things of the past.
-                                    </h3>
-                                    <h3 className='hover:text-white hover:text-xl'>
-                                        We promise not only a futuristic motorcycle, but also a futuristic
-                                        and hassle-free riding experience.
-                                    </h3>
-                                </span>
-                                <span className="block h-full animate-spin_words text-lg tracking-wide text-gray-500">
-                                    <h3 className='hover:text-white hover:text-xl'>
-                                        We believe in doing what is best for the environment.
-                                    </h3>
-                                    <h3 className='hover:text-white hover:text-xl'>
-                                        Electric is the future, and we are building for it right now.
-                                    </h3>
-                                </span>
-                                <span className="block h-full animate-spin_words text-lg tracking-wide text-gray-500">
-                                    <br />
-                                    <h3 className='hover:text-white hover:text-xl'>
-                                        No more needless part replacements. No more taking your vehicle to the
-                                        service centre every few months.
-                                    </h3>
-                                    <h3 className='hover:text-white hover:text-xl'>
-                                        Sit back and relax while we look after your motorcycle.
-                                        Rather, while the motorcycle looks after itself.
-                                    </h3>
-                                </span>
-                            </div>
-                        </div>
-                        <div className='w-2/3 h-full flex items-end relative'>
-                            {/* <div className="slider"></div> */}
-                            <Image
-                                src="/images/9.png"
-                                width={1920}
-                                height={1080}
-                                priority={true}
-                            />
-                        </div>
-                    </div>
-                    <SideActiveIndicator index={3}></SideActiveIndicator>
-                    <BottomLoader></BottomLoader>
-                </section>
-                {/* PERFORMANCE */}
-                <section className='bg-black aspect-video snap-center relative' id='performance-id'>
-                    <div className="flex h-full">
-                        <div className='w-2/3 relative'>
-                            <Image
-                                src="/images/6.png"
-                                layout='fill'
-                                priority={true}
-                            />
-                        </div>
-                        <div className='w-2/6 flex flex-col justify-center items-end pl-12 pr-20'>
-                            <div className="title mb-14">
-                                <h2 className=' font-oswald font-light text-4xl tracking-wide'>Performance</h2>
-                                <div className="underline w-10 h-0.5 rounded-xl my-2 ml-32 bg-highlight"></div>
-                            </div>
-                            <div className="contents tracking-widest text-gray-500">
-                                <h2 className='text-white text-lg'>Top Speed</h2>
-                                <h1 className='text-4xl font-light mb-8'>135Kmph</h1>
-                                <h2 className='text-white text-lg'>Ranges</h2>
-                                <h1 className='text-4xl font-light mb-8'>Takes you Places</h1>
-                                <h2 className='text-white text-lg'>0-60 kmph</h2>
-                                <h1 className='text-4xl font-light mb-8'>Before you Blink</h1>
-                                <h2 className='text-white text-lg'>0-80% charge</h2>
-                                <h1 className='text-4xl font-light mb-8'>25 mins</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <ReserveBtn></ReserveBtn>
-                    <SideActiveIndicator index={4}></SideActiveIndicator>
-                </section>
-            </main>
+                </div>
+            </section>
         </div>
     )
 }
+
+export default techstory
