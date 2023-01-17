34743faaa74dbafdb927425f2968d8b8d23e1d7f
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-01-17 21:11:12 +0530

    commit

--- components/widgets/Footer.jsx
+++ components/widgets/Footer.jsx
@@ -1,135 +1,186 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useForm, ValidationError } from '@formspree/react';
-import { toast } from 'react-toastify';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useForm, ValidationError } from "@formspree/react";
+import { toast } from "react-toastify";
 
 function Footer() {
-
-    function ContactForm() {
-        const [state, handleSubmit] = useForm("xbjbbdro");
-        if (state.succeeded) {
-            toast.success("Thanks for your Submission!", {
-                toastId: 'success1',
-            })
-            return (
-                <div className='flex justify-center items-center'>
-                    <p>Thanks for your submission</p>
-                </div>
-            )
-        }
-        return (
-            <form onSubmit={handleSubmit}>
-                <div className='flex w-full my-4'>
-                    <div>
-                        <input type="text" id="first_name" name='first_name' className="bg-black border-2 border-white text-white
-                        text-xs rounded-3xl block p-2.5 " placeholder="First Name" required />
-                    </div>
-                    <div>
-                        <input type="text" id="last_name" name='last_name' className="bg-black border-2 border-white text-white
-                        text-xs rounded-3xl block p-2.5 ml-4" placeholder="Last Name" required />
-                    </div>
-                </div>
-                <div>
-                    <input type="email" id="email" name='email' className="bg-black border-2 border-white text-white
-                        text-xs rounded-3xl w-full block p-2.5 my-4" placeholder="Email" required />
-                </div>
-                <button type="submit" disabled={state.submitting} className='bg-black border-2 border-white text-white
-                        text-xs rounded-3xl block py-2.5 px-12 my-4'>SEND</button>
-            </form>
-        )
+  function ContactForm() {
+    const [state, handleSubmit] = useForm("xbjbbdro");
+    if (state.succeeded) {
+      toast.success("Thanks for your Submission!", {
+        toastId: "success1",
+      });
+      return (
+        <div className="flex justify-center items-center">
+          <p>Thanks for your submission</p>
+        </div>
+      );
     }
-
     return (
-        <section className="bg-black w-screen flex justify-around flex-wrap" id="footer">
-            {/* NEWSLETTER */}
-            <div className='py-10 w-80'>
-                <div className="title">
-                    <h2 className=' font-oswald font-light text-2xl tracking-wide sm:text-xl'>NEWSLETTER</h2>
-                    <div className="underline w-10 h-0.5 rounded-xl my-2 mr-32 bg-highlight"></div>
-                </div>
-                <ContactForm></ContactForm>
-            </div>
-            {/* ADDRESS */}
-            <div className='py-10 px-2 w-56'>
-                <div className="title">
-                    <h2 className=' font-oswald font-light text-2xl tracking-wide sm:text-xl'>HEAD OFFICE</h2>
-                    <div className="underline w-10 h-0.5 rounded-xl my-2 mr-32 bg-highlight"></div>
-                </div>
-                <div className='mt-6 tracking-wide sm:text-sm'>
-                    <h3>NO 9, Ganapathy Nagar</h3>
-                    <h3>1st St, Navarathna Garden,</h3>
-                    <h3>Ekkatuthangal, Chennai,</h3>
-                    <h3>Tamil Nadu 600032</h3>
-                </div>
-            </div>
-            {/* CONATCT */}
-            <div className='py-10 px-2 w-52'>
-                <div className="title">
-                    <h2 className=' font-oswald font-light text-2xl tracking-wide sm:text-xl'>CONTACT</h2>
-                    <div className="underline w-10 h-0.5 rounded-xl my-2 mr-32 bg-highlight"></div>
-                </div>
-                <div className='mt-6 tracking-wide sm:text-sm'>
-                    <h3>Ph. No: +91 8925061646</h3>
-                    <h3>contact@rapteemotors.com</h3>
-                </div>
-                <div className="flex justify-between mt-4">
-                    {/* Linkedin: https://www.linkedin.com/company/rapteeenergy/?viewAsMember=true
+      <form onSubmit={handleSubmit}>
+        <div className="flex w-full my-4">
+          <div>
+            <input
+              type="text"
+              id="first_name"
+              name="first_name"
+              className="bg-black border-2 border-white text-white
+                        text-xs rounded-3xl block p-2.5 "
+              placeholder="First Name"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              id="last_name"
+              name="last_name"
+              className="bg-black border-2 border-white text-white
+                        text-xs rounded-3xl block p-2.5 ml-4"
+              placeholder="Last Name"
+              required
+            />
+          </div>
+        </div>
+        <div>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="bg-black border-2 border-white text-white
+                        text-xs rounded-3xl w-full block p-2.5 my-4"
+            placeholder="Email"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          disabled={state.submitting}
+          className="bg-black border-2 border-white text-white
+                        text-xs rounded-3xl block py-2.5 px-12 my-4"
+        >
+          SEND
+        </button>
+      </form>
+    );
+  }
+
+  return (
+    <section
+      className="bg-black w-screen flex justify-around flex-wrap"
+      id="footer"
+    >
+      {/* NEWSLETTER */}
+      <div className="py-10 w-80">
+        <div className="title">
+          <h2 className=" font-oswald font-light text-2xl tracking-wide sm:text-xl">
+            NEWSLETTER
+          </h2>
+          <div className="underline w-10 h-0.5 rounded-xl my-2 mr-32 bg-highlight"></div>
+        </div>
+        <ContactForm></ContactForm>
+      </div>
+      {/* ADDRESS */}
+      <div className="py-10 px-2 w-56">
+        <div className="title">
+          <h2 className=" font-oswald font-light text-2xl tracking-wide sm:text-xl">
+            HEAD OFFICE
+          </h2>
+          <div className="underline w-10 h-0.5 rounded-xl my-2 mr-32 bg-highlight"></div>
+        </div>
+        <div className="mt-6 tracking-wide sm:text-sm">
+          <h3>NO 9, Ganapathy Nagar</h3>
+          <h3>1st St, Navarathna Garden,</h3>
+          <h3>Ekkatuthangal, Chennai,</h3>
+          <h3>Tamil Nadu 600032</h3>
+        </div>
+      </div>
+      {/* CONATCT */}
+      <div className="py-10 px-2 w-52">
+        <div className="title">
+          <h2 className=" font-oswald font-light text-2xl tracking-wide sm:text-xl">
+            CONTACT
+          </h2>
+          <div className="underline w-10 h-0.5 rounded-xl my-2 mr-32 bg-highlight"></div>
+        </div>
+        <div className="mt-6 tracking-wide sm:text-sm">
+          <h3>Ph. No: +91 8925061646</h3>
+          <h3>contact@rapteemotors.com</h3>
+        </div>
+        <div className="flex justify-between mt-4">
+          {/* Linkedin: https://www.linkedin.com/company/rapteeenergy/?viewAsMember=true
                     Instagram: https://www.instagram.com/rapteeenergy/
                     twitter: https://twitter.com/RapteeEnergy
                     Facebook: https://www.facebook.com/rapteemotors */}
-                    <Link href={'https://www.linkedin.com/company/rapteeenergy/?viewAsMember=true'}
-                        target={"_blank"} rel={"noopener noreferrer"}>
-                        <Image
-                            src={'/images/linkedin.png'}
-                            width={25}
-                            height={25}
-                            className='cursor-pointer'
-                        />
-                    </Link>
-                    <Link href={'https://www.instagram.com/rapteeenergy/'}
-                        target={"_blank"} rel={"noopener noreferrer"}>
-                        <Image
-                            src={'/images/insta.png'}
-                            width={25}
-                            height={25}
-                            className='cursor-pointer'
-                        />
-                    </Link>
-                    <Link href={'https://twitter.com/RapteeEnergy'}
-                        target={"_blank"} rel={"noopener noreferrer"}>
-                        <Image
-                            src={'/images/twitter.png'}
-                            width={25}
-                            height={25}
-                            className='cursor-pointer'
-                        />
-                    </Link>
-                    <Link href={'https://www.facebook.com/rapteemotors'}
-                        target={"_blank"} rel={"noopener noreferrer"}>
-                        <Image
-                            src={'/images/facebook.png'}
-                            width={25}
-                            height={25}
-                            className='cursor-pointer'
-                        />
-                    </Link>
-                </div>
-            </div>
-            {/* ADDRESS */}
-            <div className='py-10 px-2 w-52'>
-                <div className="underline w-10 h-0.5 rounded-xl my-2 mr-32 bg-highlight"></div>
-                <div className='tracking-wide sm:text-sm'>
-                    <h2 className='my-2'>Privacy Policy</h2>
-                    <h2 className='my-2'>Refund Policy</h2>
-                    <h2 className='my-2'>Terms of Services</h2>
-                    <h2 className='my-2'>About Us</h2>
-                    <h2 className='my-2'>Blog</h2>
-                </div>
-                <div className="underline w-10 h-0.5 rounded-xl my-2 mr-32 bg-highlight"></div>
-            </div>
-        </section>
-    )
+          <Link
+            href={
+              "https://www.linkedin.com/company/rapteeenergy/?viewAsMember=true"
+            }
+            target={"_blank"}
+            rel={"noopener noreferrer"}
+          >
+            <Image
+              src={"/images/linkedin.png"}
+              width={25}
+              height={25}
+              className="cursor-pointer"
+            />
+          </Link>
+          <Link
+            href={"https://www.instagram.com/rapteeenergy/"}
+            target={"_blank"}
+            rel={"noopener noreferrer"}
+          >
+            <Image
+              src={"/images/insta.png"}
+              width={25}
+              height={25}
+              className="cursor-pointer"
+            />
+          </Link>
+          <Link
+            href={"https://twitter.com/RapteeEnergy"}
+            target={"_blank"}
+            rel={"noopener noreferrer"}
+          >
+            <Image
+              src={"/images/twitter.png"}
+              width={25}
+              height={25}
+              className="cursor-pointer"
+            />
+          </Link>
+          <Link
+            href={"https://www.facebook.com/rapteemotors"}
+            target={"_blank"}
+            rel={"noopener noreferrer"}
+          >
+            <Image
+              src={"/images/facebook.png"}
+              width={25}
+              height={25}
+              className="cursor-pointer"
+            />
+          </Link>
+        </div>
+      </div>
+      {/* ADDRESS */}
+      <div className="py-10 px-2 w-52">
+        <div className="underline w-10 h-0.5 rounded-xl my-2 mr-32 bg-highlight"></div>
+        <div className="tracking-wide sm:text-sm">
+          <h2 className="my-2">Privacy Policy</h2>
+          <h2 className="my-2">Refund Policy</h2>
+          <h2 className="my-2">Terms of Services</h2>
+          <h2 className="my-2">About Us</h2>
+          <Link href={"https://medium.com/@contact_97913"}>
+            <h2 className="my-2 cursor-pointer">Blog</h2>
+          </Link>
+        </div>
+        <div className="underline w-10 h-0.5 rounded-xl my-2 mr-32 bg-highlight"></div>
+      </div>
+    </section>
+  );
 }
 
-export default Footer
+export default Footer;
--- components/widgets/NavLinks.jsx
+++ components/widgets/NavLinks.jsx
@@ -1,60 +1,93 @@
-import React from 'react'
-import { IconContext } from 'react-icons/lib'
-import { IoMdClose } from 'react-icons/io'
-import Link from 'next/link'
-import scrollLock from 'scroll-lock';
+import React from "react";
+import { IconContext } from "react-icons/lib";
+import { IoMdClose } from "react-icons/io";
+import Link from "next/link";
+import scrollLock from "scroll-lock";
 import { gsap } from "gsap/dist/gsap.js";
-import * as Scroll from 'react-scroll';
-import { Link as Linky } from 'react-scroll'
+import * as Scroll from "react-scroll";
+import { Link as Linky } from "react-scroll";
 
 function NavLinks(props) {
-
-    function cloaseNavLinks() {
-        console.log("Closed the NAVLINK")
-        scrollLock.enablePageScroll()
-        gsap.fromTo(props.reference.current,
-            { opacity: 1, x: -384 },
-            {
-                opacity: 0, x: 0, duration: 0.5, ease: "ease-in",
-            },
-        )
-    }
-    return (
-        <nav className='pt-4 px-8 backdrop-blur-lg
+  function cloaseNavLinks() {
+    console.log("Closed the NAVLINK");
+    scrollLock.enablePageScroll();
+    gsap.fromTo(
+      props.reference.current,
+      { opacity: 1, x: -384 },
+      {
+        opacity: 0,
+        x: 0,
+        duration: 0.5,
+        ease: "ease-in",
+      }
+    );
+  }
+  return (
+    <nav
+      className="pt-4 px-8 backdrop-blur-lg
         bg-gray-400/50 z-50 h-100vh w-96 shadow-md top-0 -right-96 fixed 
-        sm:w-2/3' ref={props.reference}>
-            <div className="flex flex-col items-end">
-                <div className='flex items-center'>
-                    <h4>Close</h4>
-                    <IconContext.Provider value={{
-                        color: '#E0B317',
-                        size: '2rem'
-                    }}>
-                        <IoMdClose onClick={cloaseNavLinks} className='cursor-pointer' />
-                    </IconContext.Provider>
-                </div>
-                <Link href={'/home'}>
-                    <h1 className='text-3xl sm:text-xl my-8 cursor-pointer ease-in duration-100 hover:text-4xl sm:hover:text-2xl'>Home</h1>
-                </Link>
-                <Link href={'/techstory'}>
-                    <h1 className='text-3xl sm:text-xl my-8 cursor-pointer ease-in duration-100 hover:text-4xl sm:hover:text-2xl'>Tech Story</h1>
-                </Link>
-                <Link href={'/home'}>
-                    <h1 className='text-3xl sm:text-xl my-8 cursor-pointer ease-in duration-100 hover:text-4xl sm:hover:text-2xl'>OEM Solutions</h1>
-                </Link>
-                <Link href={'/culture'}>
-                    <h1 className='text-3xl sm:text-xl my-8 cursor-pointer ease-in duration-100 hover:text-4xl sm:hover:text-2xl'>Culture</h1>
-                </Link>
-                <Linky activeClass="active" to="footer" spy={true} smooth={true} offset={50}
-                    duration={1000} delay={0} onClick={cloaseNavLinks}>
-                    <h1 className='text-3xl sm:text-xl my-8 cursor-pointer ease-in duration-100 hover:text-4xl sm:hover:text-2xl'>Contact</h1>
-                </Linky>
-                <Link href={'https://apply.workable.com/raptee-energy/'}>
-                    <h1 className='text-3xl sm:text-xl my-8 cursor-pointer ease-in duration-100 hover:text-4xl sm:hover:text-2xl'>Career</h1>
-                </Link>
-            </div>
-        </nav>
-    )
+        sm:w-2/3"
+      ref={props.reference}
+    >
+      <div className="flex flex-col items-end">
+        <div className="flex items-center">
+          <h4>Close</h4>
+          <IconContext.Provider
+            value={{
+              color: "#E0B317",
+              size: "2rem",
+            }}
+          >
+            <IoMdClose onClick={cloaseNavLinks} className="cursor-pointer" />
+          </IconContext.Provider>
+        </div>
+        <Link href={"/home"}>
+          <h1 className="text-3xl sm:text-xl my-8 cursor-pointer ease-in duration-100 hover:text-4xl sm:hover:text-2xl">
+            Home
+          </h1>
+        </Link>
+        <Link href={"/techstory"}>
+          <h1 className="text-3xl sm:text-xl my-8 cursor-pointer ease-in duration-100 hover:text-4xl sm:hover:text-2xl">
+            Tech Story
+          </h1>
+        </Link>
+        <Link href={"/home"}>
+          <h1 className="text-3xl sm:text-xl my-8 cursor-pointer ease-in duration-100 hover:text-4xl sm:hover:text-2xl">
+            OEM Solutions
+          </h1>
+        </Link>
+        <Link href={"/culture"}>
+          <h1 className="text-3xl sm:text-xl my-8 cursor-pointer ease-in duration-100 hover:text-4xl sm:hover:text-2xl">
+            Culture
+          </h1>
+        </Link>
+        <Linky
+          activeClass="active"
+          to="footer"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={1000}
+          delay={0}
+          onClick={cloaseNavLinks}
+        >
+          <h1 className="text-3xl sm:text-xl my-8 cursor-pointer ease-in duration-100 hover:text-4xl sm:hover:text-2xl">
+            Contact
+          </h1>
+        </Linky>
+        <Link href={"https://apply.workable.com/raptee-energy/"}>
+          <h1 className="text-3xl sm:text-xl my-8 cursor-pointer ease-in duration-100 hover:text-4xl sm:hover:text-2xl">
+            Career
+          </h1>
+        </Link>
+        <Link href={"https://medium.com/@contact_97913"}>
+          <h1 className="text-3xl sm:text-xl my-8 cursor-pointer ease-in duration-100 hover:text-4xl sm:hover:text-2xl">
+            Blogs
+          </h1>
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
-export default NavLinks
+export default NavLinks;
