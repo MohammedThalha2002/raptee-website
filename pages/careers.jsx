bf33a51c479cb7092b83e52feb198f7629d86ca3
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-05-04 09:19:26 +0530

    commit

--- pages/careers.jsx
+++ pages/careers.jsx
@@ -10,6 +10,11 @@ function Careers() {
         boxSizing: "border-box",
       }}
     >
+      <Head>
+        <title>Raptee | Careers</title>
+        <meta name="Careers page" content="An E-motorcycle website - Careers" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <iframe
         src="https://apply.workable.com/raptee-energy/"
         style={{
