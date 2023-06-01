0475f3b3deb5c14b20a590d892674770fd12b98f
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-06-01 11:46:27 +0530

    commit

--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -46,11 +46,11 @@ function AboutUs() {
         }
       });
     }
-    aboutSectionObserver.observe(section1.current);
-    aboutSectionObserver.observe(section2.current);
-    aboutSectionObserver.observe(section3.current);
-    aboutSectionObserver.observe(section4.current);
-    aboutSectionObserver.observe(section5.current);
+    // aboutSectionObserver.observe(section1.current);
+    // aboutSectionObserver.observe(section2.current);
+    // aboutSectionObserver.observe(section3.current);
+    // aboutSectionObserver.observe(section4.current);
+    // aboutSectionObserver.observe(section5.current);
   }, []);
 
   return (
@@ -72,165 +72,212 @@ function AboutUs() {
       </Head>
       <NavBar mobile={false}></NavBar>
       <SideActiveIndicatorAboutUs index={sectionIndex} />
-      {/* WHO ARE WE - 1 */}
+      {/* SECTION - 1 */}
       <section
         id="abs-section1"
         ref={section1}
-        className="bg-black flex w-100vw relative"
+        className="bg-black flex h-[100vh] w-[100vw] relative"
       >
-        <div className="absolute top-[5%] left-[25%] text-lgreytext z-50 text-right">
-          <h1 className="text-[7rem] ">Who are</h1>
-          <h1 className="text-[8rem] font-bold leading-[3rem]">
-            WE <span className="text-highlight">?</span>
-          </h1>
+        <div className="absolute top-[8%] left-[25%] text-white z-50 text-right">
+          <h1 className="text-[5rem] ">What's our</h1>
+          <h1 className="text-[4.8rem] font-bold leading-[3rem]">STORY ?</h1>
         </div>
-        <div className="w-[40%] px-8 flex justify-center items-center">
-          <p className="w-[90%] mt-[60%] tracking-wide leading-[2.8rem] text-lgreytext text-[2rem] text-justify">
-            This isn&apos;t your typical &apos;About Us&apos; redirect but
-            rather a true narrative that drives us to revolutionize the future
-            of the automotive sector. When the concept of Raptee was initially
-            coined in 2019, the sole intention was to build a motorcycle that
-            was effortless. Reminiscing, though we may not have known how this
-            would pan out back in the day, we did see that we wanted to make a
-            difference. A difference that transforms the contemporary concept of
-            motorcycling in today&apos;s time. A change that inexorably ties
-            innovation and sustainability together. We wanted to do what we did
-            best, mindfully innovate!
+        <div className="w-[45%] px-8 flex justify-center items-center">
+          <p className="w-[90%] mt-[60%] tracking-wide leading-[2.6rem] text-lgreytext text-[1.8rem] text-justify">
+            Well, here&apos;s our tale. This isn&apos;t your typical &apos;About
+            Us&apos; redirect but rather a true narrative that drives us to
+            revolutionize the future of the automotive sector. When the concept
+            of Raptee was initially coined in 2019, the sole intention was to
+            build a motorcycle that was effortless.
           </p>
         </div>
-        <div className="w-[60%]">
+        <div className="w-[55%]">
           <Image
             src="/images/aboutUs/1.png"
             width="1138"
-            height="1662"
+            height="1165"
             priority={true}
             alt="EV-olution of electric motorcycles - New-gen innovation bike"
           />
         </div>
       </section>
-      {/* SOMEONE - 2 */}
+      {/* SECTION - 2 */}
       <section
         id="abs-section2"
-        ref={section2}
-        className="bg-black flex w-100vw relative"
+        // ref={section1}
+        className="bg-black flex h-[65vh] w-[100vw] relative"
       >
-        <div className="w-[55%] mt-12">
+        <div className="w-[45%] h-full px-8 flex justify-center items-center">
+          <p className="w-[90%] tracking-wide leading-[2.6rem] text-lgreytext text-[1.8rem] text-justify">
+            Reminiscing, though we may not have known how this would pan out
+            back in the day, we did see that we wanted to make a difference. A
+            difference that transforms the contemporary concept of motorcycling
+            in today's time. A change that inexorably ties innovation &
+            sustainability together. Someone once said, 'A team of engineers can
+            position to alter the world and not just study it, ' & we believe it
+            stands true. We wanted to do what we did best, mindfully innovate!
+          </p>
+        </div>
+        <div className="w-[55%] flex justify-center items-center">
           <Image
             src="/images/aboutUs/2.png"
-            width="715"
-            height="1300"
+            width="1700"
+            height="1200"
             priority={true}
             alt="EV-olution of electric motorcycles - New-gen innovation bike"
           />
         </div>
-        <div className="w-[45%] flex flex-col justify-between items-start">
-          <p className="w-[90%] tracking-wide leading-[2.8rem] text-lgreytext text-[2rem] text-justify">
-            Someone once said, &apos;A team of engineers can position to alter
-            the world and not just study it,&apos; and we believe it stands
-            true. Raptee is a full stack electric motorcycle startup with a very
-            strong technical moat. The company is founded by four engineers from
-            Chennai (Ex Tesla, Wipro), working on bringing a no-compromise
-            electric motorcycle to an otherwise scooter-only EV market. In
-            recent times, we&apos;ve received 4 Cr in grants from ARAI. We at
-            Raptee are on a mission to craft a motorcycle that is more than just
-            an EV; instead, an augmented machine built with an adaptive
-            machine-learning algorithm to bring you an effortless experience.
-          </p>
-          <div className="text-right text-lgreytext mb-[20%] font-mont font-light tracking-[3.5px] relative">
-            <h2 className="text-[4rem]">We are more than</h2>
-            <h2 className="text-[4rem]">just the talk!</h2>
-            <div className="border-r-4 border-b-4 border-highlight h-20 w-14 absolute right-0 bottom-0"></div>
-          </div>
-        </div>
       </section>
-      {/* BEING 60+ - 3 */}
+      {/* SECTION - 3 */}
       <section
         id="abs-section3"
-        ref={section3}
-        className="bg-black flex justify-evenly w-100vw relative"
+        // ref={section3}
+        className="bg-black flex h-[100vh] w-[100vw] relative"
       >
-        <p className="w-[50%] tracking-wide leading-[2.8rem] text-lgreytext text-[2rem] text-justify">
-          Being a 60+ member team, we bring in a portfolio of 18 patents (and
-          counting) with us, working towards a no-compromise electric motorcycle
-          for the consumers in the largest motorcycle market in the world, India
-          with plans of setting the first plant near Guindy, Chennai. We aim to
-          offer the customers, a premium experience with our cutting-edge
-          technology products and passionate people to make the mobility
-          effortless, smart and safe.
-        </p>
-        <div className="text-right text-lgreytext mb-[8rem] font-mont font-light tracking-[3.5px] relative">
-          <h2 className="text-[4rem]">We understand</h2>
-          <h2 className="text-[4rem]">that speed is fun,</h2>
-          <h2 className="text-[4rem]">but so is your</h2>
-          <h2 className="text-[4rem]">safety!</h2>
-          <div className="border-r-4 border-b-4 border-highlight h-20 w-14 absolute right-0 bottom-0"></div>
-        </div>
-      </section>
-      {/* ACCUMULATED LIFESTYLE - 4 */}
-      <section
-        id="abs-section4"
-        ref={section4}
-        className="bg-black flex w-100vw relative pb-[4rem]"
-      >
-        <div className="w-[40%] px-8 flex justify-center items-center">
-          <p className="w-[90%] tracking-wide leading-[2.8rem] text-lgreytext text-[2rem] text-justify">
-            In today&apos;s time, we have acclimated to the lifestyle where it
-            seems like there&apos;s no escape from monthly vehicle service
-            visits. But what if we told you that there is a motorcycle that is
-            truly adaptive in itself? We at Raptee believe that no one should
-            ever have to worry about vehicle maintenance, which is why our teams
-            have worked tirelessly to bring you a motorcycle that comes with no
-            fuss, no part replacement, and zero maintenance. An independently
-            designed vehicle meant to stay in shape for times to come so you can
-            sit back, unwind and let the motorcycle take care of itself.
-          </p>
-        </div>
-        <div className="w-[60%]">
+        <div className="w-[55%] flex justify-center items-center">
           <Image
             src="/images/aboutUs/3.png"
-            width="1960"
-            height="1900"
+            width="1180"
+            height="1200"
             priority={true}
             alt="EV-olution of electric motorcycles - New-gen innovation bike"
           />
         </div>
-      </section>
-      {/* PREMIUM - 5 */}
-      <section
-        id="abs-section5"
-        ref={section5}
-        className="bg-black flex w-100vw relative"
-      >
-        <div className="w-[55%] mt-12">
-          <Image
-            src="/images/aboutUs/4.png"
-            width="715"
-            height="1300"
-            priority={true}
-            alt="EV-olution of electric motorcycles - New-gen innovation bike"
-          />
-        </div>
-        <div className="w-[45%] flex flex-col justify-between items-start">
-          <p className="w-[90%] tracking-wide leading-[2.8rem] text-lgreytext text-[2rem] text-justify">
-            Our premium motorcycle is unlike any other vehicle and are built to
-            be intuitive and intelligent, with features like throttle mapping,
-            blind spot detection, and Bluetooth connectivity to keep you safe
-            and sound at all times. Raptee motorcycles have been tested with
-            real-time simulations, and we are elated to say that we have kicked
-            off tooling and expect to roll out our world-class motorcycles by
-            the end of the year. We, as a team, share a mutual eye for the brink
-            of innovation and believe that the beauty of riding is when you can
-            safely unwind, all while we look out for you and the pale blue dot.
+        <div className="w-[45%] h-full flex flex-col justify-center items-center">
+          <p className="w-[90%] tracking-wide leading-[2.6rem] text-lgreytext text-[1.8rem] text-justify">
+            The company is founded by four engineers from Chennai (Ex Tesla,
+            Wipro), working on bringing a no-compromise electric motorcycle to
+            an otherwise scooter-only EV market. In recent times, we’ve received
+            4 Cr in grants from ARAI. We at Raptee are on a mission to craft a
+            motorcycle that is more than just an EV; instead, an augmented
+            machine built with an adaptive machine-learning algorithm to bring
+            you an effortless experience.
           </p>
-          <div className="text-right text-lgreytext mb-[20%] font-mont font-light tracking-[3.5px] relative">
-            <h2 className="text-[4rem]">In virtuous, when </h2>
-            <h2 className="text-[4rem]">we mentioned </h2>
-            <h2 className="text-[4rem]">&apos;Effortless&apos;, we </h2>
-            <h2 className="text-[4rem]">meant that!</h2>
-            <div className="border-r-4 border-b-4 border-highlight h-20 w-14 absolute right-0 bottom-0"></div>
+          <div className="text-right text-lgreytext mt-[8%] font-mont font-medium tracking-[3.5px] relative">
+            <h2 className="text-[2rem]">We are more than just the talk!</h2>
+            <div className="border-r-2 border-b-2 border-highlight h-28 w-28 absolute -right-2 -bottom-6"></div>
           </div>
         </div>
       </section>
+      {/* SECTION - 4 */}
+      <section
+        id="abs-section2"
+        // ref={section1}
+        className="bg-black flex h-[100vh] w-[100vw] relative"
+      >
+        <div className="w-[45%] h-full px-8 flex justify-center items-center">
+          <p className="w-[90%] tracking-wide leading-[2.6rem] text-lgreytext text-[1.8rem] text-justify">
+            Being a 60+ member team, we bring in a portfolio of 18 patents (and
+            counting) with us, working towards a no-compromise electric
+            motorcycle for consumers in the largest motorcycle market in the
+            world, India, with plans of setting the first plant near Guindy,
+            Chennai. We aim to offer customers a premium experience with our
+            cutting-edge technology products and passionate people to make
+            mobility effortless, smart, and safe.
+          </p>
+        </div>
+        <div className="w-[55%]">
+          <Image
+            src="/images/aboutUs/4.png"
+            width="1960"
+            height="1900"
+            className="scale-90"
+            priority={true}
+            alt="EV-olution of electric motorcycles - New-gen innovation bike"
+          />
+        </div>
+      </section>
+      {/* SECTION - 5 */}
+      <section
+        id="abs-section2"
+        // ref={section1}
+        className="bg-black flex h-[100vh] w-[100vw] relative"
+      >
+        <div className="w-[45%] h-full px-8 flex justify-center items-center">
+          <p className="w-[90%] tracking-wide leading-[2.6rem] text-lgreytext text-[1.8rem] text-justify">
+            In today’s time, we have acclimated to the lifestyle where it seems
+            like there’s no escape from monthly vehicle service visits. But what
+            if we told you that there is a motorcycle that is truly adaptive in
+            itself? We at Raptee believe that no one should ever have to worry
+            about vehicle maintenance, which is why our teams have worked
+            tirelessly to bring you a motorcycle that comes with no fuss, no
+            part replacement, and zero maintenance
+          </p>
+        </div>
+        <div className="w-[55%]">
+          <Image
+            src="/images/aboutUs/5.png"
+            width="1400"
+            height="1200"
+            priority={true}
+            alt="EV-olution of electric motorcycles - New-gen innovation bike"
+          />
+        </div>
+      </section>
+      {/* SECTION - 6 */}
+      <section
+        id="abs-section3"
+        // ref={section3}
+        className="bg-black flex h-[100vh] w-[100vw] relative"
+      >
+        <div className="w-[45%] h-full flex flex-col justify-center items-center">
+          <p className="w-[90%] tracking-wide leading-[2.6rem] text-lgreytext text-[1.8rem] text-justify">
+            An independently designed vehicle meant to stay in shape for times
+            to come so you can sit back, unwind and let the motorcycle take care
+            of itself. Our premium motorcycle is unlike any other vehicle and is
+            built to be intuitive and intelligent, with features like throttle
+            mapping, blindspot detection, and Bluetooth connectivity to keep you
+            safe and sound at all times.
+          </p>
+          <div className="text-right text-lgreytext mt-[8%] font-mont font-medium tracking-[3.5px] relative">
+            <h2 className="text-[2rem]">We understand that speed is</h2>
+            <h2 className="text-[2rem]">fun, but so is your safety!</h2>
+            <div className="border-r-2 border-t-2 border-highlight h-28 w-28 absolute -right-2 -top-6"></div>
+          </div>
+        </div>
+        <div className="w-[55%]">
+          <Image
+            src="/images/aboutUs/1.png"
+            width="1138"
+            height="1165"
+            priority={true}
+            alt="EV-olution of electric motorcycles - New-gen innovation bike"
+          />
+        </div>
+      </section>
+      {/* SECTION - 7 */}
+      <section
+        id="abs-section3"
+        // ref={section3}
+        className="bg-black flex h-[100vh] w-[100vw] relative"
+      >
+        <div className="w-[55%]">
+          <Image
+            src="/images/aboutUs/7.png"
+            width="880"
+            height="900"
+            priority={true}
+            alt="EV-olution of electric motorcycles - New-gen innovation bike"
+          />
+        </div>
+        <div className="w-[45%] h-full flex flex-col justify-center items-center">
+          <p className="w-[90%] tracking-wide leading-[2.6rem] text-lgreytext text-[1.8rem] text-justify">
+            Raptee motorcycles have been tested with realtime simulations, and
+            we are elated to say that we have kicked off tooling and expect to
+            roll out our world-class motorcycles by the end of the year. We, as
+            a team, share a mutual eye for the bink of innovation and believe
+            that the beauty of riding is when you can safely unwind, all while
+            we look out for you and the pale blue dot
+          </p>
+          <div className="text-right text-lgreytext mt-[8%] font-mont font-medium tracking-[3.5px] relative">
+            <h2 className="text-[2rem]">In virtuous, when we mentioned</h2>
+            <h2 className="text-[2rem]">‘Effortless, ’ we meant that!</h2>
+            <div className="border-r-2 border-b-2 border-highlight h-28 w-28 absolute -right-2 -bottom-6"></div>
+          </div>
+        </div>
+      </section>
+      {/* oldddeee */}
+
       {/* FOOTER */}
       <Footer></Footer>
     </main>
